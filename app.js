9515e86d0aec7ced81998ea6c8e1c06410c5bf5a
sakshi0423 <sakshidhar44@gmail.com>
2024-09-23 11:08:47 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+
+secretMessage[7] = 'right';
+console.log(secretMessage[7]);
+
+secretMessage.shift();
+console.log(secretMessage[0]);
+
+secretMessage.unshift('Programming');
+console.log(secretMessage[0]);
+
+secretMessage.splice(6, 5, 'know');
+console.log(secretMessage);
+
+console.log(secretMessage.join(' '));
